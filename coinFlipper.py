ef5783d31d1c7b4a29cd7836ba89be866621e179
AntonyHamer-Teacher <antonyhamer.teacher@gmail.com>
2026-03-30 14:47:17 +1300

    Added coin flipper

---
+++ coinFlipper.py
@@ -0,0 +1,12 @@
+from random import choice
+
+def flip_a_coin():
+    coin_flip = choice(["Heads", "Tails"])
+    print(f"You flipped {coin_flip}.")
+    if coin_flip == "Heads":
+        print("You won!")
+    else:
+        print("You lost!")
+
+def print_how_to():
+    print("Flip a coin.\nHeads you win.\nTails you lose.")
